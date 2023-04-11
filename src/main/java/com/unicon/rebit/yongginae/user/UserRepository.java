586839ae1cc8b7b66eb94f71190f1d26b0913e5d
Park-EunBi <parkeb555@gmail.com>
2023-04-11 13:29:46 +0900

    FEAT: GET comment by comment_id API

---
+++ src/main/java/com/unicon/rebit/yongginae/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.unicon.rebit.yongginae.user;
+
+import com.unicon.rebit.yongginae.userComment.UserComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // comment_id로 정보들 가져오기
+    // 코멘트 아이디 입력하면 댓글 내용 가져오기 - 진행 중
+    /*
+    List<User> findByUserComments_id(Long id);
+     */
+
+}
