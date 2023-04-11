8e4aa5ac97ec69181b5b599cb75be539a65fbfd2
Park-EunBi <parkeb555@gmail.com>
2023-04-11 13:22:34 +0900

    FEAT: Create a response type

---
+++ src/main/java/com/unicon/rebit/yongginae/configure/response/CommonResponse.java
@@ -0,0 +1,17 @@
+package com.unicon.rebit.yongginae.configure.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommonResponse {
+
+    private Boolean isSuccess;
+
+    private int code;
+
+    private String message;
+}
